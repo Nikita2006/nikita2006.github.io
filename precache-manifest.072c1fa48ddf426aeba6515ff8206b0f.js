96cc7b85f6654d8f0c5abd4ffb5bb9b37a0de053
Nikita2006 <38164225+Nikita2006@users.noreply.github.com>
2019-05-06 19:35:22 +0300

    Add files via upload

---
+++ precache-manifest.072c1fa48ddf426aeba6515ff8206b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5243c75166d4708e6cf",
+    "url": "/static/js/main.2d687eef.chunk.js"
+  },
+  {
+    "revision": "7037ca7fe38297ba4806",
+    "url": "/static/js/2.570d8017.chunk.js"
+  },
+  {
+    "revision": "d5243c75166d4708e6cf",
+    "url": "/static/css/main.5cecea5e.chunk.css"
+  },
+  {
+    "revision": "7037ca7fe38297ba4806",
+    "url": "/static/css/2.234aeb70.chunk.css"
+  },
+  {
+    "revision": "842b07f0414dff8fa0020b65717b5650",
+    "url": "/index.html"
+  }
+];
